a965a2c0c9d6b9a9c1bcfaa92f057bfae89b082a
Jesús Arias <jesusariasgil1995@hotmail.com>
2018-02-27 13:58:28 +0100

    modificado fichero funciones

--- funciones.py
+++ funciones.py
@@ -1,12 +1,10 @@
 from lxml import etree
 
 def partido_tv(partido):
-	nombrelocal=doc.findall("match/local")
-	nombrevisitante=doc.finall("match/visitor")
-	hora=doc.findall("match/schedule")
-	tv=doc.findall("match/channels/name")
-	return print(nombrelocal,"-",nombrevisitante, hora, tv)
+	for i in doc.findall("/match/local"):
+		print(i.text)
 
-
-doc = etree.parse('http://apiclient.resultados-futbol.com/scripts/api/api.php?key=7171673be04cc06aa2426307d8b42836&tz=Europe/Madrid&format=xml&req=matchs&league=1&round=&order=twin&twolegged=1&year=2018')
+jornada=str("")
+url=str("http://apiclient.resultados-futbol.com/scripts/api/api.php?key=7171673be04cc06aa2426307d8b42836&tz=Europe/Madrid&format=xml&req=matchs&league=1&round="+jornada+"&order=twin&twolegged=1&year=2018")
+doc = etree.parse(url)
 lista_partidos=partido_tv(doc)
